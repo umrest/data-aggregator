e1d10f8059d474d68a29f938323a36e82fc064b4
Justin Newberry <jnewb@umich.edu>
2019-11-27 07:29:45 +0000

    fix sending data to dashboard

--- simple_router.py
+++ simple_router.py
@@ -78,7 +78,7 @@ class SimpleRouter():
                         if self.vision_dashboard_timeout >= 10:
                             self.vision_dashboard_timeout = 0
                             self.send_to_dashboard(msg)
-                if t == TYPES.DASHBOARD_OUT:
+                elif t == TYPES.DASHBOARD_OUT:
                     self.send_to_dashboard(msg)
                 else:
                     print("Invalid type: ", t)
